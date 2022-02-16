93100fb333cc7e081a2fc99c35a2cbb43a64f6a6
Jordan Kaye <jordan.kaye2@gmail.com>
2022-02-15 18:41:22 -0700

    Nord color themes and better visuals

---
+++ trayer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+killall trayer
+trayer --edge top --align right --widthtype request --padding 15 --iconspacing 5 --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x2e3440 --height 30 --distance 5 --distancefrom top &
+
+# for i in $(xrandr --listactivemonitors | tail -n +2 | awk -F: '{print $1}' | xargs); do
+#     trayer --edge top --align right --widthtype request --padding 15 --iconspacing 5 --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x2e3440 --height 30 --distance 5 --distancefrom top --monitor "$i" &
+# done
--- xmonad.hs
+++ xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Actions.UpdatePointer
+import XMonad.Config.Prime (ScreenId (..))
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.StatusBar
@@ -9,44 +10,76 @@ import XMonad.Util.EZConfig
 import XMonad.Util.Loggers
 import XMonad.Util.Ungrab
 
-grey1, grey2, grey3, grey4, cyan, orange :: String -> String
-grey1 = xmobarColor "#2B2E37" ""
-grey2 = xmobarColor "#555E70" ""
-grey3 = xmobarColor "#697180" ""
-grey4 = xmobarColor "#8691A8" ""
-cyan = xmobarColor "#8BABF0" ""
-orange = xmobarColor "#C45500" ""
+-- grey1, grey2, grey3, grey4, cyan, orange :: String -> String
+-- grey1 = xmobarColor "#2B2E37" ""
+-- grey2 = xmobarColor "#555E70" ""
+-- grey3 = xmobarColor "#697180" ""
+-- grey4 = xmobarColor "#8691A8" ""
+-- cyan = xmobarColor "#5294E2" ""
+-- orange = xmobarColor "#C45500" ""
+
+dark1 = xmobarColor "#2e3440" ""
+
+dark2 = xmobarColor "#434c5e" ""
+
+dark3 = xmobarColor "#3b4252" ""
+
+dark4 = xmobarColor "#4c566a" ""
+
+light1 = xmobarColor "#d8dee9" ""
+
+light2 = xmobarColor "#e5e9f0" ""
+
+light3 = xmobarColor "#eceff4" ""
+
+opal = xmobarColor "#8fbcbb" ""
+
+lightBlue = xmobarColor "#88c0d0" ""
+
+blue = xmobarColor "#81a1c1" ""
+
+darkBlue = xmobarColor "#5e81ac" ""
+
+red = xmobarColor "#bf616a" ""
+
+orange = xmobarColor "#d08770" ""
+
+yellow = xmobarColor "#ebcb8b" ""
+
+seaGreen = xmobarColor "#a3be8c" ""
+
+purple = xmobarColor "#b48ead" ""
 
 myXmobarPP :: PP
 myXmobarPP =
   def
     { ppSep = orange " • ",
       ppTitleSanitize = xmobarStrip,
-      ppCurrent = blue . wrap " " "" . xmobarBorder "Top" "#5294E2" 2,
-      ppHidden = white . wrap " " "",
-      ppHiddenNoWindows = lowWhite . wrap " " "",
+      ppCurrent = darkBlue . wrap " " "" . xmobarBorder "Top" "#5e81ac" 2,
+      ppHidden = light3 . wrap " " "",
+      ppHiddenNoWindows = dark4 . wrap " " "",
       ppUrgent = red . wrap (yellow "!") (yellow "!"),
       ppOrder = \[ws, l, _, wins] -> [ws, l, wins],
       ppExtras = [logTitles formatFocused formatUnfocused]
     }
   where
-    formatFocused = wrap (white "[") (white "]") . orange . ppWindow
-    formatUnfocused = wrap (lowWhite "[") (lowWhite "]") . cyan . ppWindow
+    formatFocused = wrap (light2 "[") (light2 "]") . orange . ppWindow
+    formatUnfocused = wrap (light1 "[") (light1 "]") . lightBlue . ppWindow
 
     ppWindow :: String -> String
     ppWindow = xmobarRaw . (\w -> if null w then "untitled" else w) . shorten 30
 
-    blue, lowWhite, magenta, red, white, yellow :: String -> String
-    magenta = xmobarColor "#ff79c6" ""
-    blue = xmobarColor "#5294E2" ""
-    white = xmobarColor "#7C818C" ""
-    yellow = xmobarColor "#f1fa8c" ""
-    red = xmobarColor "#ff5555" ""
-    lowWhite = xmobarColor "#4B5162" ""
+-- blue, lowWhite, magenta, red, white, yellow :: String -> String
+-- magenta = xmobarColor "#ff79c6" ""
+-- blue = xmobarColor "#5294E2" ""
+-- white = xmobarColor "#7C818C" ""
+-- yellow = xmobarColor "#f1fa8c" ""
+-- red = xmobarColor "#ff5555" ""
+-- lowWhite = xmobarColor "#4B5162" ""
 
 myStartupHook :: X ()
 myStartupHook = do
-  spawn "killall trayer; trayer --edge top --align right --widthtype request --padding 15 --iconspacing 5 --SetDockType true --SetPartialStrut true --expand true --transparent true --alpha 0 --tint 0x383C4A  --height 30 --distance 5 --distancefrom top &"
+  spawn "~/.xmonad/trayer.sh"
 
 myConfig =
   def
@@ -73,7 +106,7 @@ main =
   xmonad
     . ewmhFullscreen
     . ewmh
-    . withEasySB (statusBarProp "xmobar ~/.xmonad/xmobarrc" $ pure myXmobarPP) toggleStrutsKey
+    . dynamicEasySBs (\(S sid) -> pure $ statusBarProp ("xmobar -x " <> show sid <> " ~/.xmonad/xmobarrc") $ pure myXmobarPP)
     $ myConfig
   where
     toggleStrutsKey :: XConfig Layout -> (KeyMask, KeySym)
